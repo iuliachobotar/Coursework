dd71f9722a0250d00da2dd6fdfa014eba6010457
iuliachobotar <chobotar_ly@ukr.net>
2023-12-14 22:13:07 +0200

    edits

---
+++ button.js
@@ -0,0 +1,11 @@
+// function myImg(){
+//     let elem = document.getElementById('0');
+//     let style = getComputedStyle(elem);
+//     if (style.display === 'none') {
+//         document.getElementById('0').style.display='block';
+//     } else if (style.display !== 'none') {
+//         document.getElementById('0').style.display='none';
+//     }
+
+// }
+
